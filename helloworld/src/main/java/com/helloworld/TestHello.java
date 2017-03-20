4878371b2f88d4d7775109118b1eac2178250f17
Suprada <suprada.rao>
2017-03-20 19:35:31 +0530

    Added test file

---
+++ helloworld/src/main/java/com/helloworld/TestHello.java
@@ -0,0 +1,18 @@
+package com.helloworld;
+
+import android.util.Log;
+
+/**
+ * Created by Suprada on 20-Mar-17.
+ */
+
+public class TestHello {
+    private static final String TAG = "Welcome";
+
+    public TestHello() {
+    }
+
+    public static void g(String message) {
+        Log.d("Welcome", message);
+    }
+}
